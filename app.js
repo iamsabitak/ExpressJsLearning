ad0a2eda50fbdb3728ac34f879d69b0abbc4fad6
sabita <sabitakhadka091@gmail.com>
2024-12-03 16:31:57 +0545

    feat: Add basic Express server with routes for home, about, and contact pages

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Hello this is About page");
+});
+app.get("/contact", (req, res) => {
+  res.send("Hello this is Contact page");
+});
+
+app.listen(8000, () => {
+  console.log("Listening the port at 8000");
+});
+
+// API
+// get  --- read
+// post --- create
+// put  --- update
+// delete --- delete
+
+// The callback function has 2 parameters, request(req) and response(res).
+// The request object(reg) represents the HTTP request and has properties for the request query string, parameters, body,HTTP headers, etc.
+
+// Similarly, the response object represents the HTTP response
+// that the Express app sends when it receives an HTTP request.
